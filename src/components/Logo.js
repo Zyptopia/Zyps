367b12a8a35b16a71f712fb14d8ba50c7869675b
HKSK <hksk@gmail.com>
2025-01-14 22:15:30 +1100

    Your commit message here

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import logo from '../assets/logo.png';  // Use ../ to go up one level
+
 
 function Logo() {
   return (
     <div className="logo-container">
-      <h1 className="logo">Zyptopia</h1>
+      <img src={logo} alt="Zypto Logo" className="logo" />
     </div>
   );
 }
