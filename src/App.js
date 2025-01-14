d0edfa234c512aba477942ac49ca66a5cb2530aa
HKSK <hksk@gmail.com>
2025-01-14 18:37:57 +1100

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,38 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // Change BrowserRouter to HashRouter
-import { getAuth, onAuthStateChanged } from 'firebase/auth'; // Firebase v9+ modular imports
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import ZypsEarningsCalculator from './pages/ZypsEarningsCalculator';
 import AboutPage from './pages/AboutPage';
-import DataInputPage from './pages/DataInputPage'; // Import the data input page
-import LoginPage from './pages/LoginPage'; // Import the login page
-import DashboardPage from './pages/DashboardPage'; // Add a new dashboard page
+import DataInputPage from './pages/DataInputPage';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
 import './styles/App.css';
 
 const App = () => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const auth = getAuth(); // Get Firebase Auth instance
-    const unsubscribe = onAuthStateChanged(auth, setUser); // Listen for auth state changes
-    return () => unsubscribe(); // Clean up listener when the component is unmounted
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, setUser);
+    return () => unsubscribe();
   }, []);
 
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path="/Zyps" element={<HomePage />} />  {/* Public pages */}
+        <Route path="/Zyps" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/calculator" element={<ZypsEarningsCalculator />} />
-        <Route path="/login" element={<LoginPage />} />  {/* Login page (only accessible at /login) */}
-        
-        {/* Protected route for DataInputPage */}
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={user ? <DashboardPage /> : <Navigate to="/login" />} />
-        
-        {/* If the user is logged in, they are redirected to the dashboard */}
         <Route path="/data-input" element={user ? <DataInputPage /> : <Navigate to="/login" />} />
       </Routes>
       <Footer />
